aa2f92826ad565d4d846dcddf969af4118d47aa4
java天下第一 <1187712115@qq.com>
2018-08-13 09:37:56 +0800

    第一个免密项目

--- Demo/src/demo3.java
+++ Demo/src/demo3.java
@@ -1,5 +1,15 @@
 public class demo3 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("开始");
+        Thread.sleep(6000);//程序暂停。休眠状态
+        System.out.println(Thread.currentThread()); //输出当前线程
+        Thread thread = new Thread() {   //创建一个新线程
+            @Override
+            public void run() {
+                System.out.println("新线程");
+            }
+        };
+        thread.start(); //新线程开始执行
+        System.out.println("继续");
     }
 }
